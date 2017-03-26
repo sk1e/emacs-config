6e8b4d5995b43fa349d1b1b4d4ebc7383b10367d
Sergey Petrov <sekk1e@yandex.ru>
2017-03-26 23:56:56 +0700

    + camel-case->lisp-case utility

--- utils.el
+++ utils.el
@@ -1,5 +1,13 @@
 (defun buffer-file-name/no-extension ()
   (file-name-nondirectory (file-name-sans-extension (buffer-file-name))))
 
+(defun camel-case->lisp-case (str)
+  (let ((case-fold-search nil))
+    (mapconcat (lambda (x) (downcase (car x)))
+               (s-match-strings-all (rx (: upper-case (+ lower-case)))
+                                    str)
+               "-")))
+
+
 (provide 'utils)
 
