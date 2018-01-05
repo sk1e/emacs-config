a54d35e9ecd63fa63ae0b8496b31831022ecaeb6
Sergey Petrov <sekk1e@yandex.ru>
2018-01-05 23:57:06 +0700

    utils: + getting feature prefix for web dev

--- utils.el
+++ utils.el
@@ -8,6 +8,59 @@
                                     str)
                "-")))
 
+
+(defun project:get-cache ()
+  (cl-labels
+      ((loop
+        (dir &aux (cache-file (format "%s.project/cache.el" dir)))
+        (cond
+         ((string= dir "/") '())
+         ((file-exists-p cache-file)
+          (with-temp-buffer
+            (insert-file-contents cache-file)
+            (car (read-from-string (buffer-string-no-properties (current-buffer))))))
+         (t (loop (file-name-directory (directory-file-name dir)))))))
+    (loop (file-name-directory (buffer-file-name (current-buffer))))))
+
+(defun project:save-cache (cache)
+  (cl-labels
+      ((loop
+        (dir)
+        (cond
+         ((string= dir "/") (user-error "could not find .config directory"))
+         ((file-directory-p (format "%s.project" dir))
+          (with-temp-file (format "%s.project/cache.el" dir)
+            (insert (prin1-to-string cache))))
+         (t (loop (file-name-directory (directory-file-name dir)))))))
+    (loop (file-name-directory (buffer-file-name (current-buffer))))))
+
+(defun web-dev:get-feature-name ()
+  (cl-labels
+      ((loop
+        (dir &aux
+             (dir-file (directory-file-name dir))
+             (parent-dir (file-name-directory dir-file))
+             (parent-file-nondir (file-name-nondirectory (directory-file-name parent-dir))))
+        (cond
+         ((string= dir "/") (user-error "could not get name of feature"))
+         ((string= parent-file-nondir "features") (file-name-nondirectory dir-file))
+         (t (loop parent-dir)))))
+    (loop (file-name-directory (buffer-file-name (current-buffer))))))
+
+
+(defun yas:get-action-type (action-name)
+  (format "%s:%s" (web-dev:get-feature-prefix) (s-upcase (s-snake-case action-name))))
+
+(defun web-dev:get-feature-prefix ()
+  (let* ((cache (project:get-cache))
+         (feature-name (web-dev:get-feature-name))
+         (prefix (cdr (assoc feature-name cache))))
+    (cond
+     (prefix prefix)
+     (t (let ((new-prefix (read-string (format "Prefix for feature `%s'" feature-name))))
+          (project:save-cache (cons (cons feature-name new-prefix) cache)))))))
+
+
 (defun px-to-rem (arg)
   (interactive "n? ")
   (insert (format "%.2frem" (/ arg 14.0))))
